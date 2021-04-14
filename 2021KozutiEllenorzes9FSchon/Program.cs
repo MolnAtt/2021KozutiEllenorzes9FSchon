6e1299f387b6b9beb570ee83ee141c3bd424042f
MolnAtt <molnar.attila@szlgbp.hu>
2021-04-14 08:49:19 +0200

    Add project files.

---
+++ 2021KozutiEllenorzes9FSchon/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2021KozutiEllenorzes9FSchon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
